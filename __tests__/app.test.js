caec82cded700547aa7b3897b2c833ab23721570
AmitSheth9 <amit.sheth9@gmail.com>
2021-11-04 11:27:30 -0700

    working tests for get,getall, delete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const twilio = require('twilio');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -28,19 +29,28 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
   it('gets an order by id', async() => {
-    const res = await request(app).get('/api/v1/orders/id');
-    expect(res.body).toEqual({ id: 1, quantity: 10 });
+    const order = await Order.insert(11);
+    const res = await request(app).get('/api/v1/orders/1');
+    expect(res.body).toEqual(order);
   });
   it('gets all orders', async() => {
+    const order = await Order.insert(12);
+    const order1 = await Order.insert(13);
+    const order2 = await Order.insert(14);
     const res = await request(app).get('/api/v1/orders');
-    expect(res.body).toEqual(Array);
+    expect(res.body).toEqual([order, order1, order2]);
 
   });
   it('deletes an order by id', async() => {
-    const res = await request(app).delete('/api/v1/orders/id');
-    expect(res.body).toEqual(Object);
+    const order1 = await Order.insert(11);
+    // eslint-disable-next-line no-unused-vars
+    const order2 = await Order.insert(12);
+    const order3 = await Order.insert(13);
+    await request(app).delete('/api/v1/orders/2');
+    const getOrders = await Order.getAll();
+    expect(getOrders).toEqual([order1, order3]);
   });
-  it('updates an order by id', async() => {
+  it.skip('updates an order by id', async() => {
     const res = await request(app).patch('/api/v1/orders/id');
     expect(res.body).toEqual(Object);
   });
